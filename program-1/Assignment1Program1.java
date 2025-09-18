e18c48efb3a1b3eb20e8f22d0b04dab4271bdc6e
JaydenGaither24 <Gaitherja24@gmail.com>
2025-09-17 23:40:02 -0400

    Homework

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,63 @@
-public class Assignment1Program1 {
-  public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.Random;
 
+public class Assignment1Program1 {
+  public static void printInitials() {
+    System.out.println(" JJJJJJJJJJJJJJJJJJJJ    GGGGGGGGGGGGG   ");
+    System.out.println("         JJJJ          GGGG       GGGG  ");
+    System.out.println("         JJJJ          GGGG             ");
+    System.out.println("         JJJJ          GGGG             ");
+    System.out.println("         JJJJ          GGGG             ");
+    System.out.println("         JJJJ          GGGG     GGGG    ");
+    System.out.println("         JJJJ          GGGG       GGGGG ");
+    System.out.println("         JJJJ          GGGG        GGGG ");
+    System.out.println(" JJJ     JJJJ          GGGG        GGGG ");
+    System.out.println("  JJJ    JJJ            GGGGGGGGGGGGG   ");
+    System.out.println("    JJJJJJJJ              GGGGGGGGGGG   ");
+
+  }
+
+  public static double convertToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5.0 / 9.0;
+  }
+
+  public static String reverseString(String input) {
+    return new StringBuilder(input).reverse().toString();
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Random rand = new Random();
+
+    printInitials();
+
+    System.out.print("Enter a 5-character string: ");
+    String inputStr = scanner.nextLine();
+
+    if (inputStr.length() != 5) {
+      System.out.println("Invalid input. Please enter exactly 5 characters. ");
+      scanner.close();
+      return;
+    
+    }
+
+    String middle = inputStr.substring(1, 4);
+    String reversedMiddle = reverseString(middle);
+
+    System.out.print("Enter temperature in farenheit: ");
+    double fahrenheit = scanner.nextDouble();
+    double celsius = convertToCelsius(fahrenheit);
+
+    int randomNum = rand.nextInt(16384 - 32 + 1) + 32;
+
+    System.out.println("\n--- Final Output ---");
+    System.out.println("Reversed middle characters: " + reversedMiddle);
+    System.out.printf("Temperature in Celsius: %.2f\n", celsius);
+    System.out.println("Random number: " + randomNum);
+
+    String finalOutput = reversedMiddle + "-" + String.format("%.2f", celsius) + "-" + randomNum;
+    System.out.println("Combined Result: " + finalOutput);
+
+    scanner.close();
   }
 }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,32 @@
-public class Assignment1Program2 {
-  public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Assignment1Program2 {
+  public static String convertToBase(int number, int base) {
+if (number < base) {
+return Integer.toString(number);
+} else {
+  return convertToBase(number / base, base) +
+  (number % base);
+}
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("enter base (2-9): ");
+    int base = sc.nextInt();
+
+    int maxInBase10 = (int) (Math.pow(base, 4) - 1);
+
+    System.out.println("maximun 4 digit base 10 number in base " + base + "is" + maxInBase10 + ".");
+
+    System.out.println("enter base 10 number in the range 0 to " + "to convert: ");
+    int number = sc.nextInt();
+
+    String converted = convertToBase(number, base);
+
+    System.out.println(number + " (base 10) = " + converted + "(base " + base + ")");
+
+    sc.close();
   }
 }
